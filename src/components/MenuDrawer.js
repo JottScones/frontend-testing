38aa9bd23b3a9c26a60314a4184dd3a23ec9e5b3
scottjones <sj6018@ic.ac.uk>
2020-05-28 21:15:46 +0100

    menu drawer with icons and nice way of adding new pages. TODO: better styling with the buttons

--- src/components/MenuDrawer.js
+++ src/components/MenuDrawer.js
@@ -5,26 +5,40 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import InfoIcon from '@material-ui/icons/Info';
+import NewReleasesIcon from '@material-ui/icons/NewReleases';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 
-const drawer = (
+const drawer = () => {
+  /*To change/add options ensure that you match the index with the icons*/
+  let pages = ['Gym data', 'About', 'Covid-19 Advice', 'Contact us'];
+  let icons = [<EqualizerIcon/>, <InfoIcon/>,<NewReleasesIcon/>, <PermContactCalendarIcon/>];
+  return (
   <div>
     <Divider />
     <List>
-      {['Gym data', 'About', 'Covid-19 Advice', 'Contact us'].map((text) => (
+      <ListItem button key={"Sign up/in"}>
+        <ListItemText primary={"Sign up/in"}/>
+        <ListItemIcon><LockOpenIcon/></ListItemIcon>
+      </ListItem>
+      <Divider />
+      {pages.map((text, index) => (
         <ListItem button key={text}>
-          <ListItemIcon>{/*need to choose icon*/}</ListItemIcon>
+          <ListItemIcon>{icons[index]}</ListItemIcon>
           <ListItemText primary={text} />
         </ListItem>
       ))}
     </List>
     <Divider />
   </div>
-);
+)};
 
 const MenuDrawer = (props) => (
   <Drawer anchor="left" variant="temporary" open={props.menuOpen} onClose={() => props.setMenuOpen(false)}>
-    {drawer}
+    {drawer()}
   </Drawer>
 );
 
